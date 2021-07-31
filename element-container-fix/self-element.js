854cfcfe54d77c91fe08e4a998b0d0595be76bad
FEIZZER <2208146837@qq.com>
2021-07-31 15:11:48 +0800

    container-fix-done

--- element-container-fix/self-element.js
+++ element-container-fix/self-element.js
@@ -1,7 +1,21 @@
 /*
- * @Author: xiao-jie
+ * @Author: FEIZZER
  * @Date: 2021-07-31 11:21:15
  * @LastEditors: xiao-jie
- * @LastEditTime: 2021-07-31 11:21:16
- * @Description: 
+ * @LastEditTime: 2021-07-31 15:11:09
+ * @Description:
  */
+let pageHei = window.innerHeight
+let pageWid = window.innerWidth;
+window.addEventListener('DOMContentLoaded', onready);
+function onready(){
+  let aside = document.getElementById('main-aside')
+  let header = document.getElementById('main-header')
+  let footer = document.getElementById('main-footer')
+  let main = document.getElementById('main-main')
+  aside.style.height = pageHei + 'px'
+  header.style.width = pageWid + 'px'
+  footer.style.width = pageWid + 'px'
+  console.log(aside.style.width)
+  main.style.marginLeft = aside.style.width
+}
